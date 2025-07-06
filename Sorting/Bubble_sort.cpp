05d7020f207bcbd3500e4a00e58be00c1ab3016e
Aahaan Sethi <aahaansethi1001@gmail.com>
2025-07-06 22:00:09 +0530

    bubble sorting

--- Sorting/Bubble_sort.cpp
+++ Sorting/Bubble_sort.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void bubblesort(int arr[], int n)
 {
     for (int i = n - 1; i >= 1; i--)
-    {
+    { int didswap =0;
         for (int j = 0; j < i; j++)
         {
             if (arr[j] > arr[j + 1])
@@ -12,9 +12,13 @@ void bubblesort(int arr[], int n)
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
+                didswap = i;
             }
         }
+        if(didswap==0)
+        break;
     }
+    
 }
 
 int main()

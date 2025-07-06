935a66de4b1212235cfc2819ab13be9cc196c9fc
Aahaan Sethi <aahaansethi1001@gmail.com>
2025-07-06 23:05:10 +0530

    Quick sorting

---
+++ Sorting/Quick_sort.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int partition(vector<int>& arr, int low, int high) {
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+
+    while (i < j) {
+        while (arr[i] <= pivot && i <= high - 1) {
+            i++;
+        }
+        while (arr[j] > pivot && j >= low + 1) {
+            j--;
+        }
+        if (i < j) {
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[low], arr[j]);
+    return j;
+}
+
+void qs(vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int pIndex = partition(arr, low, high);
+        qs(arr, low, pIndex - 1);
+        qs(arr, pIndex + 1, high);
+    }
+}
+
+vector<int> quicksort(vector<int> arr) {
+    qs(arr, 0, arr.size() - 1);
+    return arr;
+}
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    vector<int> sorted = quicksort(arr);
+
+    cout << "Sorted array: ";
+    for (int num : sorted) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
